ad3b81d0ed8bf4d5adb9ac73fa9e2611c5f62721
HuangYiSheng <674845142@qq.com>
2021-07-07 11:45:11 +0800

    logo完成

--- src/components/public/Logo.vue
+++ src/components/public/Logo.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="logo">logo</div>
+  <div class="logo">
+    <!-- logo图标 -->
+    <div>
+      <h1><img src="../../assets/logo.png" alt="" /></h1>
+    </div>
+    <!-- 信息 -->
+    <div class="info">
+      <p>发现最新最热电影</p>
+      <span>｜</span>
+      <h3>打开App</h3>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,14 +18,6 @@ export default {
   data() {
     return {};
   },
-
-  components: {},
-
-  computed: {},
-
-  mounted() {},
-
-  methods: {},
 };
 </script>
 
@@ -23,5 +26,42 @@ export default {
 @import "../../assets/css/var.less";
 .logo {
   height: 60px;
+  display: flex;
+  justify-content: space-between;
+  background: white;
+  padding: 0 10px;
+  // logo图标
+  h1 {
+    height: 100%;
+    .center();
+    img {
+      width: 94px;
+      height: 39px;
+    }
+  }
+  // 信息
+  .info {
+    display: flex;
+    .center();
+    // 发现信息
+    p {
+      font-size: @xs-font;
+      font-weight: 900;
+      color: #666;
+    }
+    // 竖杠
+    span {
+      font-size: @s-font;
+      color: rgba(0, 0, 0, 0.1);
+    }
+    // 打开app
+    h3 {
+      color: #b37e7e;
+      padding-right: 10px;
+      font-size: @m-font;
+      background: url(../../assets/nav_arrow.png) no-repeat right center/10px
+        10px;
+    }
+  }
 }
 </style>
